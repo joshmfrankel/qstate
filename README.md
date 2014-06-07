a3b4c218afc7a36248b9206b8f590d5248eac645
Josh Frankel <joshmfrankel@gmail.com>
2014-06-07 14:09:24 -0400

    Josh: All Files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quint State Website  
+
+A ruby on rails website for the 2014 MFT Quint State conference.
+
+## Features to Implement
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qstate::Application.config.secret_key_base = '60c2427685b85d1714b2318888c705750489d7a567849b5191e8c665bac961fe05029f5983f26706a143f50d97507e3f024546459aedc6d2f775b5e5794b8e2f'
