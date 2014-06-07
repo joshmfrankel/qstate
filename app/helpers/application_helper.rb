fbb636e43cff33a90acbb622eb720f440d1fa9b4
Josh Frankel <joshmfrankel@gmail.com>
2014-06-07 16:53:58 -0400

    Up to end of chap 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    # Title Helper
+    def full_title(page_title)
+        base_title = "Quint State Conference at UGA"
+
+        if page_title.empty?
+            base_title
+        else
+            "#{page_title} | #{base_title}"
+        end
+    end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,9 +13,14 @@ describe "Static pages" do
             expect(page).to have_content('Home')
         end
 
-        it "should have the title 'Home'" do
+        it "should have the base title" do
           visit '/static_pages/home'
-          expect(page).to have_title("Home | #{base_title}")
+          expect(page).to have_title("#{base_title}")
+        end
+
+        it "should not have the title 'Home'" do
+          visit '/static_pages/home'
+          expect(page).not_to have_title("Home |")
         end
 
     end
