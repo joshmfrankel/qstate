b750b197f8823521674557465a443a1e80f640a9
Josh Frankel <joshmfrankel@gmail.com>
2014-06-07 16:21:59 -0400

    TDD for h1 and title. Add h1 and title to pages.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+    # Set the base title for everypage
+    let(:base_title) { "Quint State Conference at UGA" }
+
     # Home Page
     describe "Home page" do
 
@@ -12,7 +15,7 @@ describe "Static pages" do
 
         it "should have the title 'Home'" do
           visit '/static_pages/home'
-          expect(page).to have_title("Home | Quint State Conference at UGA")
+          expect(page).to have_title("Home | #{base_title}")
         end
 
     end
@@ -23,6 +26,56 @@ describe "Static pages" do
         it "should have the content 'Contact'" do
             visit '/static_pages/contact'
             expect(page).to have_content('Contact')
+        end
+
+        it "should have the title 'Contact Us'" do
+          visit '/static_pages/contact'
+          expect(page).to have_title("Contact Us | #{base_title}")
+        end
+
+    end
+
+    # FAQ
+    describe "FAQ page" do
+
+        it "should have the content 'FAQ'" do
+            visit '/static_pages/faq'
+            expect(page).to have_content('FAQ')
+        end
+
+        it "should have the title 'FAQ'" do
+          visit '/static_pages/faq'
+          expect(page).to have_title("FAQ | #{base_title}")
+        end
+
+    end
+
+    # Proposal Page
+    describe "Proposal page" do
+
+        it "should have the content 'Proposal'" do
+            visit '/static_pages/proposal'
+            expect(page).to have_content('Proposal')
+        end
+
+        it "should have the title 'Proposal'" do
+          visit '/static_pages/proposal'
+          expect(page).to have_title("Proposal | #{base_title}")
+        end
+
+    end
+
+    # Registration Page
+    describe "Registration page" do
+
+        it "should have the content 'Registration'" do
+            visit '/static_pages/registration'
+            expect(page).to have_content('Registration')
+        end
+
+        it "should have the title 'Registration'" do
+          visit '/static_pages/registration'
+          expect(page).to have_title("Registration | #{base_title}")
         end
 
     end
