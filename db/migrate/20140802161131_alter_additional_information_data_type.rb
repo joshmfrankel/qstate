f6a146e9081ea408b329e18d1fff3cb938daceea
Josh Frankel <joshmfrankel@gmail.com>
2014-08-02 13:44:57 -0400

    Alter table comment out

--- db/migrate/20140802161131_alter_additional_information_data_type.rb
+++ db/migrate/20140802161131_alter_additional_information_data_type.rb
@@ -1,5 +1,5 @@
 class AlterAdditionalInformationDataType < ActiveRecord::Migration
-  def change
-    change_column :registrations, :additional_information, :text, :limit => null
-  end
+  #def change
+  #  change_column :registrations, :additional_information, :text, :limit => null
+  #end
 end
